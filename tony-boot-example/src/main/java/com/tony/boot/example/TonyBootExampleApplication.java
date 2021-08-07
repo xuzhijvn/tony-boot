d59d527a32f71e4f5a3d40fd01caabf6f719b497
徐植君 <tony121.xu@huolala.cn>
2021-08-07 23:57:13 +0800

    mybatis二级缓存

---
+++ tony-boot-example/src/main/java/com/tony/boot/example/TonyBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tony.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonyBootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TonyBootExampleApplication.class, args);
+    }
+
+}
---
+++ tony-boot-example/src/test/java/com/tony/boot/example/TonyBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tony.boot.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonyBootExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
